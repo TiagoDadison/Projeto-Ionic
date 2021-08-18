69092d63d81d5aafd63fceaa4a1d651db54d4a1b
unknown <tiagodadison2503@gmail.com>
2021-08-18 18:08:32 -0300

    first commit

---
+++ src/app/auth/pages/login/login.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit(): void {
+    console.log('Authform: ', this.authForm.value);
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email(): FormControl {
+    return this.authForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.authForm.get('password') as FormControl;
+  }
+}
